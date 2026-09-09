6e9044e1df02ac2b4ad31826f97c011d043a27fa
JustineBatuhan <Justinebatuhan017@gmail.com>
2026-09-09 23:08:18 +0800

    update: update title in login.php

--- frontend/login.php
+++ frontend/login.php
@@ -7,7 +7,7 @@ http_response_code(404);
 <head>
     <meta charset=UTF-8>
     <meta name=viewport content=width=device-width, initial-scale=1.0>
-    <title>404 Not Found</title>
+    <title>NOENEL GWAPO</title>
     <style>
         body { 
             margin: 0; 
